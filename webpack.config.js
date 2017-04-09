5b9c17d1eefbf9404d51118d66c09b846d452105
zacck <coderv63@gmail.com>
2017-04-09 06:10:20 +0200

    work  on adding a config for webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var path = require('path');
+
+module.exports  = {
+  entry : {
+    app: [
+      './src/index.js'
+    ]
+  },
+  output: {
+    publicPath: '/',
+    filename '[name].bundle.js',
+    path: __dirname + '/public'
+  },
+  module: {
+   rules: [
+     {
+       test: /\.(css|scss)$/,
+       use: [
+         'style-loader',
+         'css-loader',
+       ]
+     },
+     {
+       test:    /\.html$/,
+       exclude: /node_modules/,
+       loader:  'file-loader?name=[name].[ext]',
+     },
+     {
+       test:    /\.elm$/,
+       exclude: [/elm-stuff/, /node_modules/],
+       loader:  'elm-webpack-loader?verbose=true&warn=true',
+     },
+     {
+       test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+       loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+     },
+     {
+       test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+       loader: 'file-loader',
+     },
+   ],
+
+   noParse: /\.elm$/,
+ },
+ devServer: {
+   contentBase: './public',
+   inline: true,
+   hot: true
+ },
+ devtool: 'source-map'
+}
