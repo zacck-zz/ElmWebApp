325278dd793f39c2e07b1fb89bc093715e56ce55
zacck <coderv63@gmail.com>
2017-04-09 07:32:28 +0200

    update Main

--- src/Main.elm
+++ src/Main.elm
@@ -18,16 +18,17 @@ init =
 
 
 -- MESSAGES
-
-
+-- think of these as lifecycle hooks
+-- these are things that our application responds to in this case we don't do anything so we ..
+-- have a message name NoOp.
 type Msg
     = NoOp
 
 
 
 -- VIEW
-
-
+-- This is a function that renders an HTml element using our application model
+-- the type signature Html Msg means this will produce messages  tagged with Msg
 view : Model -> Html Msg
 view model =
     div []
@@ -36,8 +37,9 @@ view model =
 
 
 -- UPDATE
-
-
+-- this function will be called by Html.program each time a message is receiced
+-- it responds to messages updating the model and returning commands as needed
+-- here we respond to Noop and return no command to perform
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
@@ -47,8 +49,8 @@ update msg model =
 
 
 -- SUBSCRIPTIONS
-
-
+-- We use subsriptions to listen to external input, Browser location change, keyboard events,
+-- mouse movements In our case we are not taking in any external input so we use sub.none
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
@@ -56,8 +58,8 @@ subscriptions model =
 
 
 -- MAIN
-
-
+-- Here Html.progra wires everything together and returns an html element  that we can render on our page
+-- the program takes out init,view, update and subsriptions
 main : Program Never Model Msg
 main =
     program
