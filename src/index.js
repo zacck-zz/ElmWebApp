780b714f4011e8e2ffd239b4d30a4b15ee044a99
zacck <coderv63@gmail.com>
2017-04-09 06:20:02 +0200

    ignore elm stuff

---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//import elm
+var Elm = require('./Main.elm');
+//find div or element to attach to on DOM
+var mountNode = document.getElementById('main');
+//attach our app to the dom
+var app = Elm.Main.embed(mountNode);
