df74643b790d98e59102b16528410a172963a03d
zacck <coderv63@gmail.com>
2017-04-09 07:31:59 +0200

    work on elm app skeleon

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,16 @@
+-- Declare a module named Main exposing  everthing
 module Main exposing (..)
-
+-- Import the html module and div, textm program functions from it
 import Html exposing (Html, div, text, program)
 
 
 -- MODEL
-
-
+-- This is our application model we can use this later for the data layer?
 type alias Model =
     String
-
-
+--define an init function this provides the initial input for the application
+-- Html.program expects a tuple  the first item being our intial state
+-- the sedond element being the command to run
 init : ( Model, Cmd Msg )
 init =
     ( "Hello World", Cmd.none )
